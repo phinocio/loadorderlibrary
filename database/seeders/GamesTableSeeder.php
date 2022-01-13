c099b6d7da36d3476536015aa89b400332d11386
Phinocio <github@phinocio.com>
2022-01-13 16:56:31 -0700

    fix seeder

--- database/seeders/GamesTableSeeder.php
+++ database/seeders/GamesTableSeeder.php
@@ -15,42 +15,52 @@ class GamesTableSeeder extends Seeder
     public function run()
     {
 		DB::table('games')->insert([
+			'id' => 1,
 			'name' => 'TESIII Morrowind',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 2,
 			'name' => 'TESIV Oblivion',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 3,
 			'name' => 'TESV Skyrim LE',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 4,
 			'name' => 'TESV Skyrim SE',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 5,
 			'name' => 'TESV Skyrim VR',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 6,
 			'name' => 'Fallout 3',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 7,
 			'name' => 'Fallout New Vegas',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 8,
 			'name' => 'Fallout 4',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 9,
 			'name' => 'Fallout 4 VR',
 		]);
 
 		DB::table('games')->insert([
+			'id' => 10,
 			'name' => 'Tale of Two Wastelands',
 		]);
     }
