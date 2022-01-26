d2885c59bb6b9609daceb3b5d93ff5e73fc8aca5
Phinocio <github@phinocio.com>
2022-01-25 19:40:16 -0700

    change support options; remove crypto options

--- resources/views/support-me.blade.php
+++ resources/views/support-me.blade.php
@@ -13,12 +13,10 @@
 
 				<div class="card-body">
 					<p>
-						If you like the site and want to support me, feel free to pledge on Patreon or send me some Cryptocurrency. Please don't feel obligated.
+						If you like the site and want to support me, feel free to pledge on Patreon or Ko-Fi. Please don't feel obligated.
 					</p>
 
 					<p>For the sake of transparency of expenses, please see <a href="https://github.com/phinocio/loadorderlibrary/blob/master/EXPENSES.md">EXPENSES.md</a> on Github for a breakdown of the expenses of the site.</p>
-
-					<p>If I do receive any support, I will update this page with the amount of any received Crypto, again for the sake of transparency. (Patreon would just be visible on the Patreon site itself).</p>
 				</div>
 			</div>
 		</div>
@@ -43,35 +41,11 @@
 									<td><a href="https://patreon.com/phinocio" target="_blank" rel="noopener noreferrer">https://patreon.com/phinocio</a></td>
 								</tr>
 								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('btc-address')">Copy</button></td>
-									<th scope="row">Bitcoin (BTC)</th>
-									<td id="btc-address">3JtbHZS4d69SMKsETWZQJf9VpryQNcdDCh</td>
+									<td></td>
+									<th scope="row">Ko-Fi</th>
+									<td><a href="https://ko-fi.com/phinocio" target="_blank" rel="noopener noreferrer">https://ko-fi.com/phinocio</a></td>
 								</tr>
-								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('eth-address')">Copy</button></td>
-									<th scope="row">Ethereum (ETH)</th>
-									<td id="eth-address">0x4753F54b6D00d859160cF4760A8d741B8b393F35</td>
-								</tr>
-								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('ada-address')">Copy</button></td>
-									<th scope="row">Cardano (ADA)</th>
-									<td id="ada-address">addr1v8clljrnl9z0cxwehmufcy2529e663c62dk3m5qv2qyf6kc4huk0y</td>
-								</tr>
-								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('ban-address')">Copy</button></td>
-									<th scope="row">Banano (BAN)</th>
-									<td id="ban-address">ban_3x8rr9mawggjgcqw4pqafuexbzijq1bbnzxrzusqsq6s8gox7jx4zannx8zu</td>
-								</tr>
-								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('algo-address')">Copy</button></td>
-									<th scope="row">Algorand (ALGO)</th>
-									<td id="algo-address">DXQ46ABUKYG5GZD4RGOJTQPKE5BHVEZDNB46VKK7ARKEMO4GMTBKHU4NK4</td>
-								</tr>
-								<tr>
-									<td><button type="button" class="btn btn-primary btn-sm" onclick="copyAddress('xlm-address')">Copy</button></td>
-									<th scope="row">Stellar Lumens (XLM)</th>
-									<td id="xlm-address">GDQP2KPQGKIHYJGXNUIYOMHARUARCA7DJT5FO2FFOOKY3B2WSQHG4W37:::ucl:::2291154021</td>
-								</tr>
+
 							</tbody>
 						</table>
 					</div>
