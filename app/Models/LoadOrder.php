8c6be70faf0af52e8454ab82ef002d548b2fee80
Phinocio <github@phinocio.com>
2022-01-30 23:29:57 -0700

    refactor

--- app/Models/LoadOrder.php
+++ app/Models/LoadOrder.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $author
+ */
 class LoadOrder extends Model
 {
 	use HasFactory;
