30e4062ceb7ee9746ec8d5b28946a1426d15abd9
Phinocio <github@phinocio.com>
2022-01-31 18:22:42 -0700

    make the 404 page actually decent

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,36 @@
-@extends('errors::minimal')
+@extends('layouts.app')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('title', 'Not Found')
+
+@section('content')
+
+<h1 class="text-white">404 Not Found</h1>
+
+<div class="row justify-content-center">
+	<div class="col-md-12">
+		<div class="card bg-dark col-md-12 mb-3">
+			<div class="card-header text-white">
+				<h2>The page you were looking for can not be found.</h2>
+			</div>
+			<div class="text-white card-body bg-dark m-0 pr-10">
+				This could be because of one of the following reasons
+				<ul class="list-group">
+					<li class="bg-dark text-white list-group-item list-group-item-dark">A typo in the URL</li>
+					<li class="bg-dark text-white list-group-item list-group-item-dark">The page you are looking for simply does not exist</li>
+					<li class="bg-dark text-white list-group-item list-group-item-dark">The list you are trying to view has expired and been deleted</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="card bg-dark col-md-12 mb-3">
+			<div class="card-header text-white">
+				<h2>Search Lists</h2>
+			</div>
+			<div class="text-white card-body bg-dark m-0 pr-10">
+				List search goes here once it's made
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
