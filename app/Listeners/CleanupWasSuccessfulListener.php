464dc3086ca84ff674e22a5f0b03841e128e5789
Phinocio <github@phinocio.com>
2023-06-25 14:00:17 -0600

    udpate docker to work better locally; install mariadb-client

---
+++ app/Listeners/CleanupWasSuccessfulListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class CleanupWasSuccessfulListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+    }
+}
