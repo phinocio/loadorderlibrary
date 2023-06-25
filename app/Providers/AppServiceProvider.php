2dddece0a19819f58ec31143f1a6eecebcef3ea3
Phinocio <github@phinocio.com>
2023-06-25 17:11:17 -0600

    chore: disable lazy loading in dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,28 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        // Register service providers.
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// Register service providers.
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Model::preventLazyLoading(!app()->isProduction());
+		Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
 		Paginator::useBootstrap();
-    }
+	}
 }
