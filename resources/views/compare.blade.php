caf5fa07ad8e644cce5836e5d51637b276ac3ff7
Phinocio <github@phinocio.com>
2023-08-27 12:36:16 -0600

    add link to M.E.O.W on compare page; attempt to fix display of lists on it

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -7,7 +7,14 @@
 
 	<div class="col-md-12">
 		<h1 class="text-white">Compare Load Orders (BETA)</h1>
-		
+		<div class="alert alert-info" role="alert">
+			<span>If you would like to search for a <a class="" href="https://www.wabbajack.org/" target="_blank" rel="noopener">Wabbajack
+					<x-icons.external-site />
+				</a></span> mod list based on mods you want to play with, check out <a class="" href="https://www.mauzigkeit.com/" target="_blank" rel="noopener">https://www.mauzigkeit.com/
+				<x-icons.external-site />
+			</a></span>
+		</div>
+
 		<x-compare-load-orders :loadOrders=$loadOrders />
 	</div>
 </div>
